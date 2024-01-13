583dbab2821e106327a2b964a71078d91b5667b7
balyanamod <balyanamod@gmail.com>
2024-01-13 14:59:40 +0530

    Add new file

---
+++ newfile.dart
@@ -0,0 +1,21 @@
+void main() {
+  // for loop
+  //if you need to know the index
+  for (var i = 0; i <= 20; i = i + 5) {
+    print(i);
+  }
+//for in loop
+//dont care about the index
+  List<int> num = [1, 3, 5, 7, 2, 4, 6];
+  for (var n in num) {
+    print(n);
+  }
+// for each
+  List<int> num2 = [8, 9, 7, 6, 5];
+  num2.forEach((n) {
+    print(n);
+  });
+
+// while loop
+// do while loop
+}
