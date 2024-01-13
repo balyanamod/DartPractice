415e2448a95e1194d61d998dc06b97bc0238d98a
balyanamod <balyanamod@gmail.com>
2024-01-13 21:27:51 +0530

    update

--- number_guessing.dart
+++ number_guessing.dart
@@ -6,13 +6,10 @@ void main() {
   var num1 = random.nextInt(10);
   //stdout.writeln(num1);
   stdout.writeln("Enter Your Number (0 to 10):");
-  dynamic num2 = stdin.readLineSync();
-  if (num2 != null) {
-    int num = int.parse(num2);
-    if (num1 == num) {
-      stdout.writeln("Great! Your Guessing Is Right");
-    } else {
-      stdout.writeln("Your Guessing is Wrong");
-    }
+  int? num2 = int.parse(stdin.readLineSync()!);
+  if (num1 == num2) {
+    stdout.writeln("Great! Your Guessing Is Right");
+  } else {
+    stdout.writeln("Oops! Your Guessing is Wrong");
   }
 }
