0c8302a2f86dd781affcfeaa56cf0e1bea4b4a46
balyanamod <balyanamod@gmail.com>
2024-01-13 21:19:37 +0530

    New file Added

---
+++ functions.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num = addNums(4, 5, 6);
+  print(num);
+}
+
+int addNums(int x, int y, int z) {
+  int sum = x + y + z;
+  return sum;
+}
---
+++ simple_interest.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the principle Amount:");
+  int? P = int.parse(stdin.readLineSync()!);
+  print("Enter the Time in Years:");
+  int? T = int.parse(stdin.readLineSync()!);
+  print("Enter the rate of interest in percent:");
+  int? R = int.parse(stdin.readLineSync()!);
+  double SI = (P * R * T) / 100;
+  print("Simple Interest is $SI");
+}
