2ec4fc2e53041a4feedfb0c4205baa1c9cdcb6dc
balyanamod <balyanamod@gmail.com>
2024-01-29 16:52:46 +0530

    update

--- Dart Tutorial Practice 3/q4.dart
+++ Dart Tutorial Practice 3/q4.dart
@@ -1,2 +1,8 @@
 // Write a program in Dart that generates random password.
+import 'dart:math';
 
+void main() {
+  List<int> rndList = List.generate(8, (_) => Random().nextInt(10));
+  String rndPass = rndList.join("");
+  print("Your random passwor is $rndPass");
+}
